0cc991a7486ef4a83a976bbcf621504d5ec3cc40
diyuan001 <66237308+diyuan001@users.noreply.github.com>
2021-12-17 21:41:20 +0800

    Add files via upload

--- mycpu_core.v
+++ mycpu_core.v
@@ -11,7 +11,7 @@ module mycpu_core(
     input wire [31:0] inst_sram_rdata,
 
     output wire data_sram_en,
-    output wire [3:0] data_sram_wen,
+    output wire [3:0]data_sram_wen,
     output wire [31:0] data_sram_addr,
     output wire [31:0] data_sram_wdata,
     input wire [31:0] data_sram_rdata,
@@ -28,40 +28,18 @@ module mycpu_core(
     wire [`BR_WD-1:0] br_bus; 
     wire [`DATA_SRAM_WD-1:0] ex_dt_sram_bus;
     wire [`WB_TO_RF_WD-1:0] wb_to_rf_bus;
+    wire [37:0] ex_to_id_bus;
+    wire [37:0] mem_to_id_bus;
+    wire [37:0] wb_to_id_bus;
     wire [`StallBus-1:0] stall;
-    
-    //HILO寄存器
+    wire stall_id;
+    wire inst_is_lw;
     wire stallreq_for_ex;
-    wire [7:0] lo_hi_to_ex_bus;
-    wire [31:0] hi_o;
-    wire [31:0] lo_o;
-    //wire [34:0] lo_hi_to_wb_bus;
-    wire lo_hi_we;
-    wire [31:0] hi_i;
-    wire [31:0] lo_i;
-    wire [66:0] lo_hi_ex_to_wb_bus;
-    wire WB_lo_hi_we;
-    
-    //解决数据相关！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    wire [31:0] EX_ID ;//上一条指令的结果
-    wire EX_wb_en; //上一条指令的写回使能为高
-    wire [4:0] EX_wb_r; //上一条指令的写回寄存器
-    wire EX_sel_rf_res; //解决load引起的数据相关
-    wire MEM_sel_rf_res;
-    //wire stallreq;
-    
-    //解决数据相关！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    wire [31:0] MEM_ID;//MEM段手中的运算结果
-    wire MEM_wb_en; //写回使能为高
-    wire [4:0] MEM_wb_r; //写回寄存器的索引
-    wire MEM_sel_rf_res;
-    wire stallreq_for_load;
-    wire EX_inst_div;
-    
-    //解决数据相关！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    wire [31:0] WB_ID;//MEM段手中的运算结果
-    wire WB_wb_en; //写回使能为高
-    wire [4:0] WB_wb_r; //写回寄存器的索引
+    wire [64:0] ex_mem_lohi_bus;
+    wire [64:0] mem_wb_lohi_bus;
+    wire [64:0] mem_ex_lohi_bus;
+    wire [64:0] wb_ex_lohi_bus;
+    wire [64:0] wb_id_lohi_bus;
 
     IF u_IF(
     	.clk             (clk             ),
@@ -80,50 +58,35 @@ module mycpu_core(
     	.clk             (clk             ),
         .rst             (rst             ),
         .stall           (stall           ),
-        //.stallreq        (stallreq        ),
+        .stallreq        (stall_id        ),
         .if_to_id_bus    (if_to_id_bus    ),
+        .ex_to_id_bus    (ex_to_id_bus    ),
+        .mem_to_id_bus   (mem_to_id_bus   ),
         .inst_sram_rdata (inst_sram_rdata ),
         .wb_to_rf_bus    (wb_to_rf_bus    ),
         .id_to_ex_bus    (id_to_ex_bus    ),
+        .wb_to_id_bus    (wb_to_id_bus    ),
         .br_bus          (br_bus          ),
-        .EX_ID           (EX_ID),
-        .EX_wb_en        (EX_wb_en),
-        .EX_wb_r         (EX_wb_r),
-        .EX_sel_rf_res   (EX_sel_rf_res),
-        .MEM_sel_rf_res   (MEM_sel_rf_res),
-        .MEM_ID          ( MEM_ID),
-        .MEM_wb_en       (MEM_wb_en),
-        .MEM_wb_r        (MEM_wb_r),
-        .WB_ID           (WB_ID),
-        .WB_wb_en        (WB_wb_en),
-        .WB_wb_r         (WB_wb_r),
-        .stallreq         (stallreq_for_load),
-        .lo_hi_to_ex_bus (lo_hi_to_ex_bus),
-        .EX_inst_div     (EX_inst_div),
-        .WB_lo_hi_we     (WB_lo_hi_we)
-        //.lo_hi_to_wb_bus (lo_hi_to_wb_bus)
+        .inst_is_lw      (inst_is_lw      ),
+        .wb_id_lohi_bus  (wb_id_lohi_bus  ),
+        .mem_ex_lohi_bus (mem_ex_lohi_bus ),
+        .wb_ex_lohi_bus  (wb_ex_lohi_bus  )
     );
 
     EX u_EX(
     	.clk             (clk             ),
         .rst             (rst             ),
         .stall           (stall           ),
+        .stallreq_for_ex (stallreq_for_ex ),
+        .ex_mem_lohi_bus (ex_mem_lohi_bus ),
         .id_to_ex_bus    (id_to_ex_bus    ),
         .ex_to_mem_bus   (ex_to_mem_bus   ),
+        .ex_to_id_bus    (ex_to_id_bus    ),
         .data_sram_en    (data_sram_en    ),
         .data_sram_wen   (data_sram_wen   ),
         .data_sram_addr  (data_sram_addr  ),
         .data_sram_wdata (data_sram_wdata ),
-        .EX_ID         (EX_ID),
-        .EX_wb_en        (EX_wb_en),
-        .EX_wb_r         (EX_wb_r),
-        .EX_sel_rf_res   (EX_sel_rf_res),
-        .lo_hi_to_ex_bus (lo_hi_to_ex_bus),
-        .hi_o              (hi_o),
-        .lo_o              (lo_o),
-        .lo_hi_ex_to_wb_bus (lo_hi_ex_to_wb_bus),
-        .stallreq_for_ex   (stallreq_for_ex),
-        .EX_inst_div     (EX_inst_div)
+        .inst_is_lw      (inst_is_lw      )
     );
 
     MEM u_MEM(
@@ -131,12 +94,12 @@ module mycpu_core(
         .rst             (rst             ),
         .stall           (stall           ),
         .ex_to_mem_bus   (ex_to_mem_bus   ),
+        .mem_to_id_bus   (mem_to_id_bus   ),
         .data_sram_rdata (data_sram_rdata ),
         .mem_to_wb_bus   (mem_to_wb_bus   ),
-        .MEM_ID           (MEM_ID),
-        .MEM_wb_en        (MEM_wb_en),
-        .MEM_wb_r         (MEM_wb_r),
-        .MEM_sel_rf_res   (MEM_sel_rf_res)
+        .ex_mem_lohi_bus (ex_mem_lohi_bus ),
+        .mem_wb_lohi_bus (mem_wb_lohi_bus ),
+        .mem_ex_lohi_bus (mem_ex_lohi_bus )
     );
     
     WB u_WB(
@@ -145,36 +108,21 @@ module mycpu_core(
         .stall             (stall             ),
         .mem_to_wb_bus     (mem_to_wb_bus     ),
         .wb_to_rf_bus      (wb_to_rf_bus      ),
+        .wb_to_id_bus      (wb_to_id_bus      ),
         .debug_wb_pc       (debug_wb_pc       ),
         .debug_wb_rf_wen   (debug_wb_rf_wen   ),
         .debug_wb_rf_wnum  (debug_wb_rf_wnum  ),
         .debug_wb_rf_wdata (debug_wb_rf_wdata ),
-        .WB_ID           (WB_ID),
-        .WB_wb_en        (WB_wb_en),
-        .WB_wb_r         (WB_wb_r),
-        //.lo_hi_to_wb_bus (lo_hi_to_wb_bus),
-        .lo_hi_we_o       (lo_hi_we),
-        .lo_i             (lo_i),
-        .hi_i             (hi_i),
-        .lo_hi_ex_to_wb_bus (lo_hi_ex_to_wb_bus),
-        .WB_lo_hi_we     (WB_lo_hi_we)
+        .mem_wb_lohi_bus   (mem_wb_lohi_bus   ),
+        .wb_ex_lohi_bus    (wb_ex_lohi_bus    ),
+        .wb_id_lohi_bus    (wb_id_lohi_bus    )
     );
 
     CTRL u_CTRL(
-    	.rst   (rst   ),
-        .stall (stall ),
-        .stallreq_for_load  (stallreq_for_load),
-        .stallreq_for_ex   (stallreq_for_ex)
-    );
-    
-    HILO u_HILO(
-        .clk               (clk               ),
-        .rst               (rst               ),
-        .hi_o              (hi_o),
-        .lo_o              (lo_o),
-        .we                (lo_hi_we),
-        .hi_i              (hi_i),
-        .lo_i              (lo_i)
+    	.rst      (rst     ),
+    	.stall_id (stall_id),
+    	.stallreq_for_ex (stallreq_for_ex ),
+        .stall    (stall   )
     );
     
 endmodule
